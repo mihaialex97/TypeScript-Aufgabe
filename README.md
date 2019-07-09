29222af928a40b0feb3aaaf69bbd593462d321af
Alexandru Mihai <alexandrumihai.03@gmail.com>
2019-07-09 13:41:37 +0200

    1 Commit

--- README.md
+++ README.md
@@ -1,1 +1,71 @@
-# TypeScript-Aufgabe
+# Aufgabe 11 - Shopping Clauses
+Heute geht es um die Anwendung von if-else und switch statements in Ihrem Code.
+
+## Status und Kommentare
+
+Ampelstufe | Sternchen gesamt | Anmerkungen zur Aufgabe
+------------ | ------------- | -------------
+![green](https://www.t7lab.com/images/hfu/green.png) ![yellow](https://www.t7lab.com/images/hfu/yellow.png) ![red](https://www.t7lab.com/images/hfu/red.png) | | ...
+
+## Hinweise
+- Die Unterlagen der Vorlesungen sind hier zu finden [Link](https://github.com/T7Lab/eia1-vl-typescript)
+- Letztmögliche Abgabe dieser Aufgabe: Sonntag, 20.01.2019 - EOD (End of Day === 00:00 Uhr)
+- Am Ende der Aufgaben befinden sich die allgemeinen "Spielregeln" des Praktikums, sowie das Ampelsystem und Kommentare, welche im Lauf des folgenden Tags unsererseits hinterlegt werden
+- Prüfen Sie Ihr HTML-Dokument auf W3C Konformität mit Hilfe des W3-Validators: [W3C HTML Validator](https://validator.w3.org/) und stellen Sie sicher, dass keine Errors und keine Warnings erscheinen
+- Prüfen Sie Ihr CSS-Dokument auf W3C Konformität mit Hilfe des W3-Validators: [W3C CSS Validator](http://jigsaw.w3.org/css-validator/) und stellen Sie sicher, dass keine Errors und keine Warnings erscheinen
+- Aktivieren Sie GitHub Pages damit Ihre Seite über eine URL aufrufbar ist
+
+## Einschränkungen
+Keine Einschränkungen vorgegeben.
+
+### 10.1 Skizze
+Heute wird keine Skizze benötigt.
+
+### 10.2 Störer
+Erweitern Sie Ihre Funktion writeHTML um eine konditionale Abfrage, die abhängig davon, ob ein Störer (Die Dinger um besondere Angebote anzuzeigen) gesetzt werden soll, den Störer entsprechend anzeigt oder eben nicht anzeigt. Falls nötig, müssen Sie hierfür Ihr Interface `Product` erweitern, damit jedes Produkt über ein Boolean für so einen Störer verfügt.
+
+
+### 10.3 Filter 1
+Erweitern Sie Ihren TypeScript-Code um eine Filterfunktion zur Filterung nach 4 von Ihnen festgelegten Preissegmenten, z.B. 0 – 10 € / 10 – 30 € / 30 – ... € / alle
+Die Filterung soll über verschiedene Buttons erfolgen, die Sie vorher in Ihr HTML- Dokument einbauen sollten. Ein Klick auf so einen Filter soll die Ausgabe der Produkte entsprechend filtern (also nicht auf der Konsole, sondern tatsächlich in der visuellen Darstellung).
+
+
+### 10.4 Filter 2
+Bauen Sie zwei weitere Buttons zur Filterung ein, z.B. Farbe, Größe, ...
+
+
+## Spielregeln Praktikum
+
+#### 1. Studium
+Die Bearbeitung der Aufgaben setzt intensive eigenständige Recherchen voraus. Die Arbeit mit den angegebenen Quellen ist zu üben und die darin enthaltenen Informationen sind tatsächlich intensiv zu studieren. Sie sind nicht nur wichtig für die Lösung der aktuellen Aufgabe. Experimentieren Sie weiter damit, auch wenn die gestellte Aufgabe bereits gelöst ist. Stellen Sie sich selbst weitere Aufgaben und Ziele!
+
+#### 2. Kennzeichnung Ihrer Arbeit
+Platzieren Sie zu Beginn ihres Programmcodes, außer bei Aufgabe 0, immer folgende Kommentarzeilen, wobei Sie die Texte in Klammern durch ihre persönlichen bzw. aktuellen Daten ersetzen
+```
+/*
+Aufgabe: (Nummer und Titel der Aufgabe) Name: (Ihr Name)
+Matrikel: (Ihre Matrikelnummer)
+Datum: (Datum der letzten Bearbeitung) Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert. */
+```
+Hinweis: Bei HTML-Dokumente nehmen Sie den Kommentar in den Body-Bereich und beachten Sie, wie HTML-Kommentare erstellt werden.
+
+#### 3. Quellen
+Sie dürfen sämtliche Quellen nutzen, die Ihnen zur Verfügung stehen – auch Ihre Kommilitonen und deren Arbeiten auf der Kursseite. Ein reger Wissensaustausch wird empfohlen, ebenso wie die Bildung von Lerngruppen. Nicht akzeptiert wird allerdings die unreflektierte Übernahme fremden Gedankengutes.
+
+#### 4. Hilfestellung
+Bei technischen Problemen fragen und antworten Sie im Forum der Veranstaltung. Dieses wird auch von den Betreuern und dem Professor überwacht und beantwortet. Wenden Sie sich bei persönlichen Problemen, beispielsweise bei einem nicht selbst zu verantwortendem Verzug bei der Abgabe, direkt an Ihre Betreuer. Falls die Probleme auf dieser Ebene nicht gelöst werden können, wenden Sie sich an den Lehrbeauftragten.
+
+#### 5. Konzeption
+Beginnen Sie die Arbeit an jeder Aufgabe mit einem Konzept. Dieses enthält immer eine oder mehrere Skizzen, eine Beschreibung der Lösung und eine Analyse, bei der bereits die wichtigsten Algorithmen aufgezeigt werden. Halten Sie sich immer Papier und Bleistift bereit, um auch während des Praktikums zu planen und zu skizzieren.
+
+#### 6. Kommentare
+Schreiben Sie sinnvolle Kommentare in Ihren Code und sparen Sie nicht daran. Sie machen es sich und Ihren Betreuern damit leichter sich zu Recht zu finden.
+
+#### 8. Vorbereitung
+Versuchen Sie sich - sofern die Aufgaben vorher publiziert wurden - für das Praktikum vorzubereiten. Eine Skizze kann hier sehr hilfreich sein. Diese soll Ihnen helfen, die eigentliche Aufgabe zu verstehen und Ihren möglichen Lösungsansatz im Voraus und frei von jeglicher Technik anzugehen.
+
+#### 8. GitHub Classroom
+In der README eines jeden Projekts wird Ihre Ampelstufe gepflegt. Beachten Sie, dass Ihre aktuelle Ampelstufe immer in der aktuellsten Aufgabe zu finden ist.
+Ist eine Lösung zur aktuellen Aufgabe zum Abgabetermin nicht in Ihrem Repository, wird die Ampel um eine Stufe hochgeschaltet, die nicht mehr zurückgenommen wird.
+Eine unzureichende Lösung oder ein Plagiatsverdacht wiederum hat die Schaltung einer halben Ampelstufe zur Folge. Sie erhalten dazu die Aufforderung, Ihre Arbeit im folgenden Praktikum vor dem Betreuer zu verteidigen. Sofern Sie dabei die erforderliche Kompetenz nachweisen können, wird die Ampel um die halbe Ampelstufe zurückgeschaltet.
+Erreicht die Ampel die Stufe rot, führt dies zur Disqualifikation.
---
+++ js/main.ts
@@ -0,0 +1,352 @@
+interface Products {
+    name: string;
+    bezeichung: string;
+    preis: number;
+    available: number;
+    discount: number;
+    image:string;
+    disturber:boolean;
+    team:string;
+}
+
+var article1: Products = {
+    name: "Los Angeles Lakers Hat",
+    bezeichung: "hat1",
+    preis: 15,
+    available: 10,
+    discount: 0,
+    image:'img/hat1.jpg',
+    disturber:false,
+    team:"lakers"
+
+}
+
+var article2: Products = {
+    name: "Golden State Hat",
+    bezeichung: "hat2",
+    preis: 18,
+    available: 15,
+    discount: 10,
+    image:'img/hat2.jpg',
+    disturber:false,
+    team:"golden"
+}
+
+var article3: Products = {
+    name: "Chicago Bulls Hat",
+    bezeichung: "hat3",
+    preis: 20,
+    available: 12,
+    discount: 0,
+    image:'img/hat3.jpg',
+    disturber:false,
+    team:"bulls"
+}
+var article4: Products = {
+    name: "New York Nicks Hat",
+    bezeichung: "hat4",
+    preis: 10,
+    available: 10,
+    discount: 0,
+    image:'img/hat4.jpg',
+    disturber:false,
+    team:"nicks"
+}
+var article5: Products = {
+    name: "Boston Celtics Hat",
+    bezeichung: "hat5",
+    preis: 15,
+    available: 10,
+    discount: 0,
+    image:'img/hat5.jpg',
+    disturber:false,
+    team:"celtics"
+}
+var article6: Products = {
+    name: "Golden State Warriors Shirt ",
+    bezeichung: "Shirt1",
+    preis: 35,
+    available: 10,
+    discount: 0,
+    image:'img/hoodie1.jpg',
+    disturber:true,
+    team:"golden"
+}
+var article7: Products = {
+    name: "Philadephia 76ers Shirt",
+    bezeichung: "Shirt2",
+    preis: 35,
+    available: 10,
+    discount: 0,
+    image:'img/hoodie2.jpg',
+    disturber:true,
+    team:"76ers"
+}
+var article8: Products = {
+    name: "Toronto Raptors Shirt",
+    bezeichung: "Shirt3",
+    preis: 35,
+    available: 10,
+    discount: 0,
+    image:'img/hoodie3.jpg',
+    disturber:true,
+    team:"raptors"
+}
+var article9: Products = {
+    name: "Los Angeles Lakers Shirt",
+    bezeichung: "Shirt4",
+    preis: 35,
+    available: 10,
+    discount: 0,
+    image:'img/hoodie4.jpg',
+    disturber:true,
+    team:"lakers"
+}
+var article10: Products = {
+    name: "Los Angeles Lakers Shirt 2",
+    bezeichung: "Shirt5",
+    preis: 30,
+    available: 10,
+    discount: 0,
+    image:'img/hoodie5.jpg',
+    disturber:true,
+    team:"lakers"
+}
+var article11: Products = {
+    name: "Los Angeles Lakers T-Shirt",
+    bezeichung: "t-shirt1",
+    preis: 20,
+    available: 10,
+    discount: 0,
+    image:'img/shirt1.jpg',
+    disturber:true,
+    team:"lakers"
+}
+var article12: Products = {
+    name: "Boston Celtics T-Shirt",
+    bezeichung: "t-shirt2",
+    preis: 20,
+    available: 10,
+    discount: 0,
+    image:'img/shirt2.jpg',
+    disturber:true,
+    team:"celtics"
+}
+var article13: Products = {
+    name: "Milwaukee Bucks T-Shirt",
+    bezeichung: "t-shirt3",
+    preis: 20,
+    available: 10,
+    discount: 0,
+    image:'img/shirt3.jpg',
+    disturber:true,
+    team:"bucks"
+}
+var article14: Products = {
+    name: "Golden State Warriors T-Shirt",
+    bezeichung: "t-shirt4",
+    preis: 20,
+    available: 10,
+    discount: 0,
+    image:'img/shirt4.jpg',
+    disturber:true,
+    team:"golden"
+}
+var article15: Products = {
+    name: "Philadelphia 76ers T-Shirt",
+    bezeichung: "t-shirt5",
+    preis: 20,
+    available: 10,
+    discount: 0,
+    image:'img/shirt5.jpg',
+    disturber:true,
+    team:"76ers"
+}
+var article16: Products = {
+    name: "Los Angeles Lakers Socks",
+    bezeichung: "sock1",
+    preis: 10,
+    available: 10,
+    discount: 0,
+    image:'img/sock1.jpg',
+    disturber:false,
+    team:"lakers"
+}
+var article17: Products = {
+    name: "Boston Celtics Socks",
+    bezeichung: "sock2",
+    preis: 10,
+    available: 10,
+    discount: 0,
+    image:'img/sock2.jpg',
+    disturber:false,
+    team:"celtics"
+}
+var article18: Products = {
+    name: "Philadelphia 76ers Socks",
+    bezeichung: "sock3",
+    preis: 10,
+    available: 10,
+    discount: 0,
+    image:'img/sock3.jpg',
+    disturber:false,
+    team:"76ers"
+}
+var article19: Products = {
+    name: "Los Angeles Lakers Socks 2",
+    bezeichung: "sock4",
+    preis: 10,
+    available: 10,
+    discount: 0,
+    image:'img/sock4.jpg',
+    disturber:false,
+    team:"lakers"
+}
+var article20: Products = {
+    name: "Denver Nuggets",
+    bezeichung: "sock5",
+    preis: 10,
+    available: 10,
+    discount: 0,
+    image:'img/sock5.jpg',
+    disturber:false,
+    team:"nuggets"
+}
+
+
+var allProducts: Products[]=[article1,article2,article3,article4,article5,article6,article7,article8,article9,article10,article11,article12,article13,article14,article15,article16,article17,article18,article19,article20];
+var cart: Products[] = [];
+
+
+
+// funtion for adding to cart
+function addToCart(item: Products) {
+    cart.push(item);
+    console.log(cart);
+    calculateSumCart(cart);
+    numberCart();
+}
+// function to calculate the entire cart
+function calculateSumCart(cart: Products[]) {
+    var sum = 0;
+    for (var i = 0; i < cart.length; i++) {
+        sum += cart[i].preis;
+    }
+    console.log(sum);
+    document.getElementById('summe').innerHTML = sum.toString() + "$";
+}
+// function that shows how many items you have in your cart
+function numberCart() {
+    document.getElementById('qty').innerHTML = cart.length.toString();
+}
+// function to empty your cart
+function deleteButton() {
+    cart = [];
+    numberCart();
+    document.getElementById('summe').innerHTML = "0$";
+}
+
+
+function renderProducts(x: Products[]): void {
+    var i: number = 0;
+    while (i < x.length) {
+        writeHtml(x[i]);
+        console.log(x[i].name);
+        i++;
+    }
+}
+
+document.addEventListener('DOMContentLoaded', init);
+
+function init() {
+    renderProducts(allProducts);
+
+}
+
+function writeHtml(pro: Products) {
+    // mit append child mein css wird nicht mehr optimal funktionieren wurde
+    
+
+    if(pro.disturber==true){
+        document.getElementById('productWrapper').innerHTML +=`<div class="produs">
+        <img src=${pro.image}  class="thumbnail" alt=${pro.bezeichung}>
+        <br>
+        <p class="pret">
+        ${pro.preis}$
+        </p>
+        <img src="img/sale.png" class="sale" alt="saletag">
+        <button type="button" class="btn btn-danger" onclick="addToCart(article1)">Kaufen</button>
+    
+        </div>`;
+    }else{
+        document.getElementById('productWrapper').innerHTML +=`<div class="produs">
+    <img src=${pro.image}  class="thumbnail" alt=${pro.bezeichung}>
+    <br>
+    <p class="pret">
+    ${pro.preis}$
+    </p>
+    <button type="button" class="btn btn-danger" onclick="addToCart(article1)">Kaufen</button>
+    </div>`;
+    }
+}
+
+function filter1(){
+    var filter1array:Products[] = [];
+    document.getElementById('productWrapper').innerHTML="";
+    for(var i:number =0;i<allProducts.length;i++){
+        if(allProducts[i].preis<=10){
+            filter1array.push(allProducts[i]);
+        }
+    }
+    renderProducts(filter1array);
+}
+function filter2(){
+    var filter1array:Products[] = [];
+    document.getElementById('productWrapper').innerHTML="";
+    for(var i:number =0;i<allProducts.length;i++){
+        if(allProducts[i].preis>=10 && allProducts[i].preis<=30){
+            filter1array.push(allProducts[i]);
+        }
+    }
+    renderProducts(filter1array);
+}
+function filter3(){
+    var filter1array:Products[] = [];
+    document.getElementById('productWrapper').innerHTML="";
+    for(var i:number =0;i<allProducts.length;i++){
+        if(allProducts[i].preis>=30){
+            filter1array.push(allProducts[i]);
+        }
+    }
+    renderProducts(filter1array);
+}
+
+function filtersize(){
+    var filter1array:Products[] = [];
+    document.getElementById('productWrapper').innerHTML="";
+    for(var i:number =0;i<allProducts.length;i++){
+        if(allProducts[i].disturber==true){
+            filter1array.push(allProducts[i]);
+        }
+    }
+    renderProducts(filter1array);
+}
+function lakers(){
+    var filter1array:Products[] = [];
+    document.getElementById('productWrapper').innerHTML="";
+    for(var i:number =0;i<allProducts.length;i++){
+        if(allProducts[i].team=='lakers'){
+            filter1array.push(allProducts[i]);
+        }
+    }
+    renderProducts(filter1array);
+}
+
+function showall(){
+    var filter1array:Products[] = [];
+    document.getElementById('productWrapper').innerHTML="";
+    for(var i:number =0;i<allProducts.length;i++){
+            filter1array.push(allProducts[i]);
+    }
+    renderProducts(filter1array);
+}
